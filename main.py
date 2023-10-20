fe37be0663335e2cfe8e46746e87821e80aa5c25
Sergei Sidorov <s.sidorov1990@mail.ru>
2023-10-20 14:10:25 +0300

    Add command start and help

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import logging
 from aiogram.utils import executor
 
 from create_bot import dp
+from user.handlers import hand_start
+
+async def on_start(_):
+    hand_start.register_handlers_start_help(dp)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_start)
---
+++ user/handlers/hand_start.py
@@ -0,0 +1,26 @@
+from aiogram.types import Message
+from aiogram import Dispatcher
+
+from create_bot import dp, bot
+
+async def start(message: Message):
+    if message.values['chat']['type'] == 'private':
+        await bot.send_message(
+            message.from_user.id,
+            f'Привет! {message.values["from"]["first_name"]}'
+            )
+    else:
+        await message.answer('Общение с ботом происходит через личные сообщения!\n https://t.me/RentAppartmentTelegrammBot')
+
+async def help(message: Message):
+    if message.values['chat']['type'] == 'private':
+        await bot.send_message(
+            message.from_user.id,
+            '/start - запустить бота или вернуться в главное меню \n/help - информация по командам'
+            )
+    else:
+        await message.answer('Общение с ботом происходит через личные сообщения!\n https://t.me/RentAppartmentTelegrammBot')
+
+def register_handlers_start_help(dp: Dispatcher):
+    dp.register_message_handler(start, commands=['start'])
+    dp.register_message_handler(help, commands=['help'])
